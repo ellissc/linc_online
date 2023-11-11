7a484f4e8af79ef7a1043e89198521fc946ce05e
Ellis Cain <50685260+ellissc@users.noreply.github.com>
2023-11-11 04:40:37 -0800

    Initial commit

---
+++ verb_cloze_stimuli/convert.R
@@ -0,0 +1,56 @@
+## Reading libraries ----
+library(here)
+setwd(here())
+library(tidyverse)
+library(jsonlite)
+
+
+## Reading data ----
+stimuli <- read_csv(here("verb_cloze_stimuli.csv")) |> 
+  distinct(question_num, question, verb, animal, .keep_all = T) |> 
+  mutate(animal = gsub("\\.","",animal))
+
+## Question num ----
+# Not sure about the question number, there are multiple questions per question num.
+
+# How often does each verb occur?
+stimuli |> 
+  group_by(verb) |> 
+  summarize(count = n())
+
+# Checking the co-occurrence of question and verb.
+table(qn = stimuli$question_num, verb = stimuli$verb) |> 
+  data.frame() |> 
+  ggplot(aes(x = qn, y = verb, fill = Freq))+
+  geom_tile()+
+  theme_bw()+
+  xlab("Question number")
+
+## For now, we'll create a new question index.
+
+stimuli <- stimuli |> 
+  mutate(index = 1:n())
+
+
+## Create a json ----
+# Index = index, inner json of other variables
+
+# Very inefficient way to do this, but R doesn't normally use JSON.
+
+full.json = list()
+
+for(i in 1:nrow(stimuli)){
+  row = stimuli[i,]
+  temp.json <- list()
+  key <- as.character(row$index)
+  temp.json[[key]] <- list(question = row$question,
+                           verb = row$verb,
+                           animal = row$animal,
+                           norm_category = row$norm_category)
+  full.json[[row$index]] <- temp.json
+}
+
+
+json_str <- toJSON(full.json, auto_unbox = TRUE)
+
+writeLines(json_str, con = "updated_stimuli.json")
